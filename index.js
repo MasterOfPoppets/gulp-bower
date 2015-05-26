8b52f20a19335a286712116191c4ee87226e124f
Gareth Hughes <gurrkin@googlemail.com>
2015-05-26 18:06:03 +0100

    [Update] Move to through2

--- index.js
+++ index.js
@@ -1,68 +1,62 @@
-var Transform = require('stream').Transform,
-	util = require('util'),
-	fs = require('fs'),
+var	fs = require('fs'),
 	gutil = require('gulp-util'),
-	toposort = require('toposort')
+	toposort = require('toposort'),
+	through = require('through2')
 
-var BowerStream = function (options) {
-	this.bowerFiles = {}
-	this.options = options || {}
-	this.options.objectMode = true
+module.exports = function (options) {
+	var bowerFiles = {}
 
-	Transform.call(this, options)
-}
-util.inherits(BowerStream, Transform)
+	function transform(chunk, encoding, next) {
+		var bowerFile = JSON.parse(chunk.contents.toString('utf8'))
+		bowerFiles[bowerFile.name] = bowerFile
+		next()
+	}
 
-BowerStream.prototype._transform = function (chunk, encoding, next) {
-	var bowerFile = JSON.parse(chunk.contents.toString('utf8'))
-	this.bowerFiles[bowerFile.name] = bowerFile
-	next()
-}
+	function flush(done) {
+		var self = this,
+			dependencyGraph = [],
+			standaloneModules = []
 
-BowerStream.prototype._flush = function () {
-	var self = this,
-		dependencyGraph = [],
-		standaloneModules = []
+		Object.keys(bowerFiles).forEach(function (module) {
+			var bowerFile = bowerFiles[module]
+			if (bowerFile.dependencies) {
+				var moduleDependencyKeys = Object.keys(bowerFile.dependencies)
 
-	Object.keys(self.bowerFiles).forEach(function (module) {
-		var bowerFile = self.bowerFiles[module]
-		if (bowerFile.dependencies) {
-			var moduleDependencyKeys = Object.keys(bowerFile.dependencies)
-
-			moduleDependencyKeys.forEach(function (moduleDependencyKey) {
-				dependencyGraph.push([bowerFile.name, moduleDependencyKey])
-			})
-		} else {
-			standaloneModules.push(bowerFile.name)
-		}
-	})
-
-	var orderedDependencies = toposort(dependencyGraph).reverse()
-	orderedDependencies.forEach(function (dependency) {
-		var standaloneModuleIndex = standaloneModules.indexOf(dependency)
-		if (standaloneModuleIndex !== -1) {
-			standaloneModules.splice(standaloneModuleIndex, 1)
-		}
-	})
-	orderedDependencies = standaloneModules.concat(orderedDependencies)
-
-	orderedDependencies.forEach(function (module) {
-		var bowerFile = self.bowerFiles[module]
-		var mainFiles = bowerFile.main
-		if (typeof mainFiles === 'string') {
-			mainFiles = [mainFiles]
-		}
-		mainFiles.forEach(function (filepath) {
-			if (self.options.excluded.indexOf(bowerFile.name) === -1) {
-				self.push(new gutil.File({
-					path: 'bower_components/' + bowerFile.name + '/' + filepath,
-					contents: fs.readFileSync('bower_components/' + bowerFile.name + '/' + filepath)
-				}))
+				moduleDependencyKeys.forEach(function (moduleDependencyKey) {
+					dependencyGraph.push([bowerFile.name, moduleDependencyKey])
+				})
+			} else {
+				standaloneModules.push(bowerFile.name)
 			}
 		})
-	})
-}
 
-module.exports = exports = function (options) {
-	return new BowerStream(options)
+		var orderedDependencies = toposort(dependencyGraph).reverse()
+		orderedDependencies.forEach(function (dependency) {
+			var standaloneModuleIndex = standaloneModules.indexOf(dependency)
+			if (standaloneModuleIndex !== -1) {
+				standaloneModules.splice(standaloneModuleIndex, 1)
+			}
+		})
+		orderedDependencies = standaloneModules.concat(orderedDependencies)
+
+		orderedDependencies.forEach(function (module) {
+			var bowerFile = bowerFiles[module]
+			var mainFiles = bowerFile.main
+			if (typeof mainFiles === 'string') {
+				mainFiles = [mainFiles]
+			}
+			mainFiles.forEach(function (filepath) {
+				if (options.excluded.indexOf(bowerFile.name) === -1) {
+					self.push(new gutil.File({
+						path: bowerFile.name + '/' + filepath,
+						contents: fs.readFileSync('bower_components/' + bowerFile.name + '/' + filepath)
+					}))
+				}
+			})
+		})
+
+		done()
+	}
+
+	return through.obj(transform, flush)
 }
