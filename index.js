af028d6adf77746a9e73b584c0ed63f2c4ccc212
Gareth Hughes <gurrkin@googlemail.com>
2015-08-29 12:47:44 +0100

    [Update] Fixing non standard bower package name bug

--- index.js
+++ index.js
@@ -1,96 +1,108 @@
-var	_ = require('highland'),
-	fs = require('fs'),
-	gutil = require('gulp-util'),
-	toposort = require('toposort'),
-	through = require('through2')
+var _ = require('highland');
+var fs = require('fs');
+var gutil = require('gulp-util');
+var toposort = require('toposort');
+var through = require('through2');
 
-module.exports = function (options) {
-	var stream,
-		options = options || {},
-		fileDescriptors = []
+module.exports = function (passedOptions) {
+	var stream;
+	var options = passedOptions || {};
+	var fileDescriptors = [];
 
-	stream = through.obj(transform, flush)
+	stream = through.obj(transform, flush);
 
 	function transform(file, encoding, next) {
 		if (file.isNull()) {
-			next()
-			return
+			next();
+			return;
 		}
 
-		fileDescriptors.push(file)
-		next()
+		fileDescriptors.push(file);
+		next();
 	}
 
 	function flush(done) {
-		_(fileDescriptors)
-			.through(parseAllFileContents)
-			.flatMap(function (file) {
-				return _.pairs(_.get('dependencies', file))
+		parsedFiledDescriptors()
+			.flatMap(function createDependencyGraph(file) {
+				return _.keys(_.get('dependencies', file))
 					.map(function (dependency) {
-						return [file.name, dependency[0]]
-					})
+						return [file.name, dependency];
+					});
 			})
-			.toArray(pushFiles)
+			.collect()
+			.map(function orderDependencies(dependencyGraph) {
+				return toposort(dependencyGraph).reverse();
+			})
+			.each(pushFiles);
+
+		done();
+
+		function parsedFiledDescriptors() {
+			return _(fileDescriptors)
+				.through(parseAllFileContents);
+		}
 
 		function parseAllFileContents(allFiles) {
 			return allFiles
 				.map(_.get('contents'))
 				.map(function (contents) {
-					return contents.toString('utf8')
+					return contents.toString('utf8');
 				})
-				.map(JSON.parse)
+				.map(JSON.parse);
 		}
 
-		function pushFiles(dependencyGraph) {
-			var orderedDependencies = toposort(dependencyGraph).reverse()
-
-			_(orderedDependencies)
-				.each(function (dep) {
-					_(fileDescriptors)
-						.through(parseAllFileContents)
-						.map(function (fileContents) {
-							return fileContents.name
-						})
-						.toArray(function (x) {
-							if (x.indexOf(dep) === -1) {
-								stream.emit('error', new gutil.PluginError('gulp-bower-mf', 'Missing dependency ' + dep))
-							}
-						})
-				})
+		function pushFiles(orderedDependencies) {
+			findMissingDependencies(orderedDependencies);
 
 			_(fileDescriptors)
-				.flatMap(function (file) {
+				.flatMap(function extractFileMeta(file) {
 					return _([file])
 						.through(parseAllFileContents)
 						.map(function (fileContents) {
-							return { path: file.path, name: fileContents.name, main: fileContents.main }
+							return {
+								base: file.base,
+								name: fileContents.name,
+								main: fileContents.main
+							};
+						});
+				})
+				.sortBy(function sortFilesIntoDependencyOrder(a, b) {
+					return orderedDependencies.indexOf(a.name) > orderedDependencies.indexOf(b.name);
+				})
+				.each(function pushFile(fileMeta) {
+					_(typeof fileMeta.main === 'string' ? [fileMeta.main] : fileMeta.main)
+						.filter(function filterExcluded() {
+							return !options.excluded || options.excluded.indexOf(fileMeta.name) === -1;
 						})
-				})
-				.sortBy(function (a, b) {
-					return orderedDependencies.indexOf(a.name) > orderedDependencies.indexOf(b.name)
-				})
-				.each(pushFile)
+						.each(function (mainFile) {
+							var file = fileMeta.base + fileMeta.name.toLowerCase() + '/' + mainFile;
+
+							stream.push(new gutil.File({
+								path: file,
+								contents: fs.readFileSync(file)
+							}));
+						});
+				});
 		}
 
-		function pushFile(fileMeta) {
-			var mainFiles = typeof fileMeta.main === 'string' ? [fileMeta.main] : fileMeta.main
-
-			mainFiles.forEach(function (mainFile) {
-				if (!options.excluded || options.excluded.indexOf(fileMeta.name) === -1) {
-					var path = fileMeta.path
-					var pathSubStr = path.substr(0, path.indexOf(fileMeta.name))
-					var file = pathSubStr + fileMeta.name + '/' + mainFile
-
-					stream.push(new gutil.File({
-						path: file,
-						contents: fs.readFileSync(file)
-					}))
-				}
-			})
+		function findMissingDependencies(orderedDependencies) {
+			_(orderedDependencies)
+				.each(function (dependency) {
+					return parsedFiledDescriptors()
+						.map(_.get('name'))
+						.collect()
+						.filter(function (allFileNames) {
+							return allFileNames.indexOf(dependency) === -1;
+						})
+						.each(function (fileMissingDependency) {
+							stream.emit('error', new gutil.PluginError(
+								'gulp-bower-mf',
+								'Missing dependency ' + dependency + ' for library ' + fileMissingDependency
+							));
+						});
+				});
 		}
-
-		done()
 	}
 
-	return stream
-}
+	return stream;
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,55 +1,62 @@
-var chai = require('chai'),
-	gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	assert = require('stream-assert'),
-	bower = require('../index.js')
+var chai = require('chai');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var assert = require('stream-assert');
+var bower = require('../index.js');
 
-chai.should()
+chai.should();
 
 describe('gulp-bower', function () {
 	it('should ignore null files', function (done) {
-		var stream = bower()
+		var stream = bower();
 
 		stream
 			.pipe(assert.length(0))
-			.pipe(assert.end(done))
+			.pipe(assert.end(done));
 
-		stream.write(new gutil.File)
-		stream.end()
-	})
+		stream.write(new gutil.File());
+		stream.end();
+	});
 
 	it('should pipe out main files in dependency order', function (done) {
-		gulp.src(['./test/fixtures/**/*.json', '!./test/fixtures/testFile3/*.json'])
+		gulp.src('./test/fixtures/standard/**/*.json')
 			.pipe(bower())
 			.pipe(assert.length(2))
 			.pipe(assert.first(function (data) {
-				data.contents.toString().should.equal('console.log(\'file2\')')
+				data.contents.toString().should.contain('console.log(\'file2\');');
 			}))
-			.pipe(assert.end(done))
-	})
+			.pipe(assert.end(done));
+	});
 
 	it('should pipe out main files without excluded components', function (done) {
-		gulp.src(['./test/fixtures/**/*.json', '!./test/fixtures/testFile3/*.json'])
+		gulp.src('./test/fixtures/standard/**/*.json')
 			.pipe(bower({
 				excluded: ['testFile1']
 			}))
 			.pipe(assert.length(1))
-			.pipe(assert.end(done))
-	})
+			.pipe(assert.end(done));
+	});
 
 	it('should pipe out all the main files for a given component', function (done) {
-		gulp.src('./test/fixtures/testFile3/*.json')
+		gulp.src('./test/fixtures/multiple_main_files/**/*.json')
 			.pipe(bower())
 			.pipe(assert.length(2))
-			.pipe(assert.end(done))
-	})
+			.pipe(assert.end(done));
+	});
 
 	it('should emit an error if a dependency is not available', function (done) {
-		gulp.src('./test/fixtures/testFile1/*.json')
+		gulp.src('./test/fixtures/missing_dependency/**/*.json')
 			.pipe(bower())
 			.on('error', function (error) {
-				error.message.should.equal('Missing dependency testFile2')
-				done()
-			})
-	})
-})
+				error.message.should.equal('Missing dependency testFile2 for library testFile1');
+				done();
+			});
+	});
+
+	it('should cope with non-standard package names', function (done) {
+		gulp.src('./test/fixtures/non_standard_package_name/**/*.json')
+			.pipe(bower())
+			.pipe(assert.length(1))
+			.pipe(assert.end(done));
+	});
+});
